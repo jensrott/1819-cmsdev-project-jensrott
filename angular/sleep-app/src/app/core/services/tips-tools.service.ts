67345fc90a2629418f1e047d5ec0c1ab59c9998d
jensrott <jensrott@student.arteveldehs.be>
2019-08-23 14:22:10 +0200

    added filtering

--- angular/sleep-app/src/app/core/services/tips-tools.service.ts
+++ angular/sleep-app/src/app/core/services/tips-tools.service.ts
@@ -5,12 +5,16 @@ import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class TipsToolsService {
+  // TODO: clean this up, make it more clear
   private _apiAllTipsTools =
     environment.wpAPI.url + environment.wpAPI.endPoints.tips_tools;
   // private _apiSpecificTipTool = environment.wpAPI.url + environment.wpAPI.endPoints.tips_tools;
   private _apiSpecificTipTool = "http://localhost/wp-json/wp/v2/tips-tools";
   private _apiAddTipsTool =
     environment.wpAPI.url + environment.wpAPI.endPoints.tips_tools;
+
+  private _apiFilterTipsTools =
+    "http://localhost/wp-json/wp/v2/tips-tools?author=";
 
   private object: Object;
 
@@ -44,6 +48,22 @@ export class TipsToolsService {
     return this._httpClient.post(
       `${this._apiAddTipsTool}`,
       this.object,
+      httpOptions
+    );
+  }
+
+  // Endpoint: http://localhost/wp-json/wp/v2/tips-tools?author={id}
+  filterByAuthor(id): Observable<any> {
+    const token = localStorage.getItem("token");
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "Access-Control-Allow-Origin": "POST"
+      })
+    };
+    return this._httpClient.get(
+      `${this._apiFilterTipsTools}${id}`,
       httpOptions
     );
   }
--- angular/sleep-app/src/app/tips-tools/pages/tips-page/tips-page.component.ts
+++ angular/sleep-app/src/app/tips-tools/pages/tips-page/tips-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { TipsToolsService } from "../../../core/services/tips-tools.service";
 
+import * as _ from "lodash";
+
 @Component({
   selector: "app-tips-page",
   templateUrl: "./tips-page.component.html",
@@ -12,13 +14,16 @@ export class TipsPageComponent implements OnInit {
   tips_tools;
   users;
 
+  // filter data by author
+  filteredDatas;
+  newFilteredData: boolean = false;
+
   show: boolean = true;
 
   constructor(private tipstoolsService: TipsToolsService) {}
 
   ngOnInit() {
     this.getAllTipsTools();
-    // console.log(this.tips_tools);
   }
 
   searchTipsTools(term) {}
@@ -26,10 +31,12 @@ export class TipsPageComponent implements OnInit {
   getAllTipsTools() {
     this.tipstoolsService.getAllTipsTools().subscribe(
       data => {
-        this.tips_tools = data;
-        console.log(this.tips_tools);
-        console.log(this.tips_tools[0].title);
-        this.tips_tools1 = this.tips_tools;
+        let sortedData = _.orderBy(data, ["date_gmt"], ["asc"]); // Sort by date
+        console.log(sortedData);
+        this.tips_tools = sortedData;
+        // console.log(this.tips_tools);
+        // console.log(this.tips_tools[0].title);
+        // this.tips_tools1 = this.tips_tools;
       },
       err => {
         console.log(err);
@@ -56,4 +63,14 @@ export class TipsPageComponent implements OnInit {
   }
 
   calculateMostLikes() {}
+
+  filterByAuthor(id: number) {
+    // console.log(id); // now we use this id to filter and we make a new request
+    this.newFilteredData = true;
+    this.tipstoolsService.filterByAuthor(id).subscribe(data => {
+      console.log(data);
+      let sortedData = _.orderBy(data, ["date_gmt"], ["asc"]); // Sort by date, TODO: find a way I can put this in one place so I don't need to place this always again
+      this.filteredDatas = sortedData; // Now we have to replace all the data that is now there with this data
+    });
+  } // Can make it later sort by anything else
 }
